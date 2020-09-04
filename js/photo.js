65f7d6f24eb5f460660c047fddb70ecee03d7160
子非鱼 <63336918+SMile-lucky@users.noreply.github.com>
2020-09-05 00:11:26 +0800

    Add files via upload

---
+++ js/photo.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.tab div img').click(function() {
+        $('.pic img').eq($(this).index()).css({ 'opacity': '1' }).siblings().css({ 'opacity': '0' });
+    });
+
+});
