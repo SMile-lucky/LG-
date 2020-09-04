ed888fcc980dc9c9f568eaf7e37682effbfe94c5
子非鱼 <63336918+SMile-lucky@users.noreply.github.com>
2020-09-04 23:45:47 +0800

    Create sp.js

---
+++ js/sp.js
@@ -0,0 +1,19 @@
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('b a(){0 6=1.2("9").4;0 5=1.2("3");0 3=1.2("3").c;0 8=5.e[3].4;0 7=1.2("f");7.d=8+6}',16,16,'var|document|getElementById|jk|value|jkurl|dj|jxurl|jkt|url|jx|function|selectedIndex|src|options|play'.split('|'),0,{}))
+
+		
+
+		function jx2() {
+
+		    var dj = document.getElementById("url").value;
+
+		    var jkurl = document.getElementById("jk");
+
+		    var jk = document.getElementById("jk").selectedIndex;
+
+		    var jkt = jkurl.options[jk].value;
+
+		    var jxurl = document.getElementById("play");
+
+		    window.open(jkt + dj,"_blank");
+
+		}
